2f442427862523ef9badb89e7b2892f40462acd8
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-08-26 17:32:11 +0530

    Get and Post of Warehouses endpoint done

---
+++ drizzle/0003_broken_changeling.sql
@@ -0,0 +1,1 @@
+CREATE INDEX "pincode_idx" ON "warehouses" USING btree ("pincode");
---
+++ src/app/api/warehouses/route.ts
@@ -0,0 +1,50 @@
+import { db } from "@/lib/db/db"
+import { warehouses } from "@/lib/db/schema"
+import { warehouseSchema } from "@/lib/validators/warehouseSchema"
+import { desc } from "drizzle-orm";
+
+
+export async function POST(req:Request) {
+  try {
+    
+    const requestData=await req.json()
+      const validated = warehouseSchema.safeParse(requestData);
+
+    if (!validated.success) {
+      return Response.json(
+        { message: "Validation failed", errors: validated.error.flatten() },
+        { status: 400 }
+      );
+    }
+
+    const {name,pincode}=validated.data
+   let t=await db.insert(warehouses).values({
+    name:name,
+    pincode:pincode
+   })
+
+  
+   
+   return Response.json({message:"ok"},{status:201})
+
+  } catch (error) {
+    console.log(error);
+    
+    return Response.json({message:error},{status:500})
+  }
+
+
+
+}
+
+export async function GET() {
+  try {
+    
+    const data=await db.select().from(warehouses).orderBy(desc(warehouses.id))
+    return Response.json({message:"ok",data},{status:200})
+
+  } catch (error) {
+    return Response.json({message:error},{status:500})
+  }
+  
+}
