2612a3c80ff2e512a2b0adde354c45315bfffdfc
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-08-28 16:34:09 +0530

    GetAllDeliveryPersons

--- src/app/api/delivery-person/route.ts
+++ src/app/api/delivery-person/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPerson } from "@/lib/db/schema";
+import { deliveryPerson, warehouses } from "@/lib/db/schema";
 import { deliveryPersonSchema } from "@/lib/validators/deliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export  async function POST(req:Request) {
 try {
@@ -17,6 +18,30 @@ try {
   
   return Response.json({
     message:"created"
+  },{status:201})
+
+} catch (error) {
+  return Response.json({
+    message:"Something went wrong",
+    error
+  },{
+    status:500
+  })
+}
+  
+}
+export  async function GET() {
+try {
+ 
+const data=  await db.select({id:deliveryPerson.id,
+  name:deliveryPerson.name,
+  phone:deliveryPerson.phone,
+  warehouse:warehouses.name
+}).from(deliveryPerson).leftJoin(warehouses,eq(deliveryPerson.id,warehouses.id)).orderBy(desc(deliveryPerson.id));
+  
+  return Response.json({
+    message:"ok",
+    data
   },{status:201})
 
 } catch (error) {
